118e014ccbdef8ca49200a448e20bf60d9eea9db
Ariel Ayala <arielayalamail@gmail.com>
2020-02-16 18:27:26 -0300

    Controlar la navegación con guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { IntroGuard } from './guards/intro.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  {
+    path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomePageModule),
+    canActivate: [IntroGuard]
+  },
   {
     path: 'intro',
-    loadChildren: () => import('./intro/intro.module').then( m => m.IntroPageModule)
+    loadChildren: () => import('./intro/intro.module').then(m => m.IntroPageModule)
   },
 ];
 
