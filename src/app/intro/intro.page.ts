2fed3e8aa525955bbb5511c13f4c0568441b96ef
Ariel Ayala <arielayalamail@gmail.com>
2020-02-16 18:02:40 -0300

    Adding store

--- src/app/intro/intro.page.ts
+++ src/app/intro/intro.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-intro',
@@ -41,11 +42,11 @@ export class IntroPage implements OnInit {
     icon: "play"
   }]
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private storage: Storage) { }
 
   finish() {
+    this.storage.set('isIntroShowed', true)
     this.router.navigateByUrl("/home")
-    console.log("HOla")
   }
 
   ngOnInit() {
