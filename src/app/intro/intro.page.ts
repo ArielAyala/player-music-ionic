50199082a16e8bac0f7a61d78f94bea1087d7771
Ariel Ayala <arielayalamail@gmail.com>
2020-02-16 12:30:02 -0300

    Adding routes

---
+++ src/app/intro/intro.page.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-intro',
+  templateUrl: './intro.page.html',
+  styleUrls: ['./intro.page.scss'],
+})
+export class IntroPage implements OnInit {
+
+  slideOps = {
+    initialSlide: 0,
+    slidesPerView: 1,
+    centerSlides: true,
+    speed: 400
+  }
+
+  slides = [{
+    imageSrc: "assets/img/logo.png",
+    imageAlt: "Platzi music logo",
+    title: "Escucha tu música",
+    subTitle: "EN CUALQUIER LUGAR",
+    description: `Los mejores álbumes, las mejores canciones. 
+    Escucha y comparte en cualquier momento, a todas horas`,
+    icon: "play"
+  }, {
+    imageSrc: "assets/img/logo.png",
+    imageAlt: "Platzi music logo",
+    title: "Disfruta de nuestro reproductor",
+    subTitle: "DE VIDEOS INCREÍBBLES",
+    description: `Entra al modo video de nuestro reproductor. <
+    Obtén acceso a los documentales y making offs increíbles de tu artista favorito.`,
+    icon: "videocam"
+  }, {
+    imageSrc: "assets/img/logo.png",
+    imageAlt: "Platzi music logo",
+    title: "Accedé al exclusivo",
+    subTitle: "MODO DEPORTE",
+    description: `Crea un playlist basado en tu actividad física. 
+    Ten reportes y acceso a lo que necesitas integrado con GPS`,
+    icon: "play"
+  }]
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
