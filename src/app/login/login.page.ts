cff2a35d965ec47364bbbb9308b24d156c154c51
Ariel Ayala <arielayalamail@gmail.com>
2020-02-16 19:24:17 -0300

    Validate login forms inputs

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -9,14 +9,40 @@ import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms'
 export class LoginPage implements OnInit {
 
   loginForm: FormGroup;
+  validation_messages = {
+    email: [{
+      type: "required",
+      message: "El email es requerido"
+    },
+    {
+      type: "pattern",
+      message: "No es un email válido"
+    }
+    ],
+    password: [{
+      type: "required",
+      message: "El password es requerido"
+    },
+    {
+      type: "minlength",
+      message: "Mínimo 5 letras para el password"
+    }]
+  }
 
   constructor(private formBuilder: FormBuilder) {
+
     this.loginForm = this.formBuilder.group({
       email: new FormControl("", Validators.compose([
         Validators.required,
         Validators.pattern("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$")
+      ])),
+      password: new FormControl("", Validators.compose([
+        Validators.required,
+        Validators.minLength(5)
       ]))
     })
+
+
   }
 
   ngOnInit() {
