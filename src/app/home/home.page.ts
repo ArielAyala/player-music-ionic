9b212c97a26978fe9cd99c7d0eaa5781d8309462
Ariel Ayala <arielayalamail@gmail.com>
2020-02-16 12:13:28 -0300

    Slides dinámicos

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  slideOps = {
+    initialSlide: 0,
+    slidesPerView: 1,
+    centerSlides: true,
+    speed: 400
+  }
 
-  constructor() {}
+  slides = [{
+    imageSrc: "assets/img/logo.png",
+    imageAlt: "Platzi music logo",
+    title: "Escucha tu música",
+    subTitle: "EN CUALQUIER LUGAR",
+    description: `Los mejores álbumes, las mejores canciones. 
+    Escucha y comparte en cualquier momento, a todas horas`,
+    icon: "play"
+  }, {
+    imageSrc: "assets/img/logo.png",
+    imageAlt: "Platzi music logo",
+    title: "Disfruta de nuestro reproductor",
+    subTitle: "DE VIDEOS INCREÍBBLES",
+    description: `Entra al modo video de nuestro reproductor. <
+    Obtén acceso a los documentales y making offs increíbles de tu artista favorito.`,
+    icon: "videocam"
+  }, {
+    imageSrc: "assets/img/logo.png",
+    imageAlt: "Platzi music logo",
+    title: "Accedé al exclusivo",
+    subTitle: "MODO DEPORTE",
+    description: `Crea un playlist basado en tu actividad física. 
+    Ten reportes y acceso a lo que necesitas integrado con GPS`,
+    icon: "play"
+  }]
+  constructor() { }
 
 }
