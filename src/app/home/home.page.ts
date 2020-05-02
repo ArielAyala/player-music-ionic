182deba35e6f15a3d7df7f0f26d02f0bfafa9846
Ariel Ayala <arielayalamail@gmail.com>
2020-05-02 18:00:05 -0400

    Detalles de duracion a reproductor

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -20,7 +20,8 @@ export class HomePage {
   albums: any[] = [];
   artists: any[] = [];
   song = {};
-
+  currentSong = {};
+  newTime;
   constructor(private musicService: PlatziMusicService, private modalController: ModalController) { }
 
   ionViewDidEnter() {
@@ -47,10 +48,32 @@ export class HomePage {
   }
 
   play() {
+    this.currentSong = new Audio(this.song.preview_url);
+    this.currentSong.play();
+    this.currentSong.addEventListener("timeupdate", () => {
+      this.newTime = (this.currentSong.currentTime * (this.currentSong.duration / 10)) / 100;
+    })
     this.song.playing = true;
   }
   pause() {
+    this.currentSong.pause();
     this.song.playing = false;
 
   }
+
+  parseTime(time = "0.00") {
+    if (time) {
+      const parTime = parseInt(time.toString().split('.')[0], 10);
+      let minutes = Math.floor(parTime / 60).toString();
+      if (minutes.length == 1) {
+        minutes = "0" + minutes;
+      }
+      let seconds = (parTime % 60).toString();
+      if (seconds.length == 1) {
+        seconds = "0" + seconds;
+      }
+      return minutes + ':' + seconds;
+    }
+  }
+  
 }
