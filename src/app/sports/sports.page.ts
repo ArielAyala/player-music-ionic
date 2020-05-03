2fc28746176bc407c3af9a86cf3b49005c4b0f77
Ariel Ayala <arielayalamail@gmail.com>
2020-05-03 14:37:47 -0400

    Utilidades en angular maps

--- src/app/sports/sports.page.ts
+++ src/app/sports/sports.page.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { Plugins } from '@capacitor/core';
+
+const { Geolocation } = Plugins;
 
 @Component({
   selector: 'app-sports',
   templateUrl: './sports.page.html',
   styleUrls: ['./sports.page.scss'],
 })
-export class SportsPage implements OnInit {
+export class SportsPage {
+
+  currentCenter: any;
+  coordinates: any[] = [];
+  defaultZoom = 14;
 
   constructor() { }
 
-  ngOnInit() {
+  ionViewDidEnter() {
+    this.getCurrentPosition();
+    this.watchPosition();
+  }
+
+  async getCurrentPosition() {
+    const coordinates = await Geolocation.getCurrentPosition();
+    this.currentCenter = {
+      lat: coordinates.coords.latitude,
+      lng: coordinates.coords.longitude
+    }
+  }
+
+  watchPosition() {
+    Geolocation.watchPosition({}, position => {
+      this.currentCenter = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      this.coordinates.push({
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      });
+    });
   }
 
 }
